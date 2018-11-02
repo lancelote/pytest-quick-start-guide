a7740b65f33b9dd60ada00368f7949b453fe338d
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-11-02 09:19:25 +0300

    Add pytest run configurations

---
+++ src/chapter2/test_last_failed.py
@@ -0,0 +1,23 @@
+def test_regex_matching():
+    pass
+
+
+def test_check_options():
+    assert 0
+
+
+def test_type_checking():
+    assert 0
+
+
+def test_parse_expr():
+    pass
+
+
+def test_parse_num():
+    pass
+
+
+def test_parse_add():
+    pass
+
